54ad544f416a59f142cf4884f83499f540be3f37
birme <jonas.birme@eyevinn.se>
2019-02-25 17:38:12 +0100

    Begin implementation of parsing child boxes

--- example.js
+++ example.js
@@ -2,6 +2,7 @@ const fMP4 = require("./index.js");
 const fs = require("fs");
 
 const TESTFILE = "./test/support/testassets/video.dash";
+//const TESTFILE = "/Users/jobi/Downloads/Sunrise.mp4";
 
 fs.createReadStream(TESTFILE)
 .pipe(fMP4.parse({ debug: false }))
@@ -11,6 +12,14 @@ fs.createReadStream(TESTFILE)
 .on("finish", function() {
   let boxes = fMP4.boxes;
   for (let i = 0; i < boxes.length; i++) {
-    console.log(boxes[i].hdr); 
+    let parsedBox = boxes[i].parse();
+    if (parsedBox.moov) {
+      console.log(parsedBox);
+      for(let j = 0; j < parsedBox.moov.children.length; j++) {
+        console.log(parsedBox.moov.children[j]);
+      }
+    } else {
+      console.log(parsedBox);
+    }
   }
 })
