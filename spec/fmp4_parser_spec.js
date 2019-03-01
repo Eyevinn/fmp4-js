3e1b435b2667c0927c859bf1a276e5b119868a96
birme <jonas.birme@eyevinn.se>
2019-03-01 10:22:06 +0100

    Use MP4Box class when unit testing

--- spec/fmp4_parser_spec.js
+++ spec/fmp4_parser_spec.js
@@ -9,39 +9,46 @@ function hexToBytes(hex) {
 }
 
 describe("MP4 Box Parser", () => {
-  let boxParser;
+  let MP4Box;
   beforeAll(() => {
     const parser = new PARSER();
-    boxParser = parser._boxParser;
+    MP4Box = parser.MP4Box;
   });
 
   it("can parse an 'ftyp' box", () => {
     let byteArray = hexToBytes('000000186674797069736f360000000069736f3664617368');
-    let data = new Uint8Array(byteArray.slice(8));
-    let box = boxParser['ftyp'](data);
-    expect(box.major_brand).toEqual('iso6');
-    expect(box.minor_version).toEqual(0);
-    expect(box.compatible_brands).toEqual(['iso6', 'dash']);
+    let data = new Uint8Array(byteArray);
+    let box = new MP4Box(data.length, 'ftyp');
+    box.data = data.slice(8);
+    let b = box.parse();
+    expect(b.ftyp.major_brand).toEqual('iso6');
+    expect(b.ftyp.minor_version).toEqual(0);
+    expect(b.ftyp.compatible_brands).toEqual(['iso6', 'dash']);
   });
 
   it("can parse an 'styp' box", () => {
     let byteArray = hexToBytes('000000187374797069736f360000000069736f366d736468');
-    let data = new Uint8Array(byteArray.slice(8));
-    let box = boxParser['styp'](data);
-    expect(box.major_brand).toEqual('iso6');
-    expect(box.minor_version).toEqual(0);
-    expect(box.compatible_brands).toEqual(['iso6', 'msdh']);
+    let data = new Uint8Array(byteArray);
+    let box = new MP4Box(data.length, 'styp');
+    box.data = data.slice(8);
+    let b = box.parse();
+    expect(b.styp.major_brand).toEqual('iso6');
+    expect(b.styp.minor_version).toEqual(0);
+    expect(b.styp.compatible_brands).toEqual(['iso6', 'msdh']);
   });
 
   it("can parse an 'sidx' box", () => {
     let byteArray = hexToBytes('0000002c73696478000000000000000100007530000000000000000000000001001003db0000e2ca90000000');
-    let data = new Uint8Array(byteArray.slice(8));
-    let box = boxParser['sidx'](data); 
-    expect(box.timescale).toEqual(30000);
-    expect(box.entries.length).toEqual(1);
-    expect(box.entries[0].reference_type).toEqual(0);
-    expect(box.entries[0].reference_size).toEqual(1049563);
-    expect(box.entries[0].subsegment_duration).toEqual(58058);
-    expect(box.entries[0].starts_with_SAP).toEqual(1);
+    let data = new Uint8Array(byteArray);
+    let box = new MP4Box(data.length, 'sidx');
+    box.data = data.slice(8);
+    let b = box.parse();
+    expect(b.sidx.version).toEqual(0);
+    expect(b.sidx.timescale).toEqual(30000);
+    expect(b.sidx.entries.length).toEqual(1);
+    expect(b.sidx.entries[0].reference_type).toEqual(0);
+    expect(b.sidx.entries[0].reference_size).toEqual(1049563);
+    expect(b.sidx.entries[0].subsegment_duration).toEqual(58058);
+    expect(b.sidx.entries[0].starts_with_SAP).toEqual(1);
   });
 });
